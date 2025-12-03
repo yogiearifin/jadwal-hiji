f0d94d273f7cba962265d859a4b238f5e00daaaa
Yogie Arifin <yogiearifinp@gmail.com>
2025-12-03 16:54:52 +0800

    reorder list alpabethically

--- src/controllers/customerController/index.ts
+++ src/controllers/customerController/index.ts
@@ -3,7 +3,7 @@ import { supabase } from "../../config/supabaseClient";
 import { errorHandler } from "../../middleware/errorMiddleware";
 
 export const getAllCustomers = async (req: Request, res: Response) => {
-  const { data, error } = await supabase.from("customer").select("*");
+  const { data, error } = await supabase.from("customer").select("*").order("name");
   if (error) {
     errorHandler(error, res);
   }
