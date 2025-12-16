185d10709b40f123d6678f2c768eba30cb0569d2
Yogie Arifin <yogiearifinp@gmail.com>
2025-12-16 16:16:57 +0800

    update region routes

--- src/routes/regionRoutes/index.ts
+++ src/routes/regionRoutes/index.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getAllRegion } from "../../controllers/regionController";
+import {
+  getAllRegion,
+  getSingleRegion,
+} from "../../controllers/regionController";
 
 const router = Router();
 
 router.get("/", getAllRegion);
+router.get("/:id", getSingleRegion);
+
 export default router;
